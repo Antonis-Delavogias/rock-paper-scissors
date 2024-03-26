ef5481b0a4b6f53dda43bd751a8900d87e457a4a
AntonisDela <antonisdelavogias@icloud.com>
2024-03-26 18:06:48 +0200

    created a function that auto generates an index of the array for my rock paper scissor game

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-'use strict'
+"use strict";
+
+const gameChoices = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
